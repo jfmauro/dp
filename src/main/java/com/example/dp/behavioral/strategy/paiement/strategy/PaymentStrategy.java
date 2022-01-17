1bb50cda82f0bd304202e23663a969eadfbd4536
MAURO JEAN-FRANCOIS <jeanfrancois.mauro@minfin.fed.be>
2022-01-17 14:00:08 +0100

    added strategy pattern

---
+++ src/main/java/com/example/dp/behavioral/strategy/paiement/strategy/PaymentStrategy.java
@@ -0,0 +1,5 @@
+package com.example.dp.behavioral.strategy.paiement.strategy;
+
+public interface PaymentStrategy {
+	public void pay(int amount);
+}
--- src/main/java/com/example/dp/structural/flyweight/builder/ChemicalBuilder.java
+++ src/main/java/com/example/dp/structural/flyweight/builder/ChemicalBuilder.java
@@ -3,10 +3,15 @@ package com.example.dp.structural.flyweight.builder;
 import com.example.dp.structural.flyweight.entity.Chemical;
 import com.example.dp.structural.flyweight.entity.Substance;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class ChemicalBuilder {
     private Map<String, Chemical> items;
+
+    public ChemicalBuilder(){
+        items = new HashMap<>();
+    }
 
 
     public Substance getSubstance (String name, Double atomicweight, String symbol, Double grams){
